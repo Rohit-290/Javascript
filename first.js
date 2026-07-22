f16e9257b49dd02b656b4fa25e4bf404576da529
Rohit-290 <rohit290kumar@gmail.com>
2026-07-22 12:38:33 +0530

    Updated on 22nd July 2026

--- first.js
+++ first.js
@@ -1,2 +1,83 @@
 console.log("Learning Javascript");
 console.log("Rohit kumar");
+firstName="Donald";
+age=34;
+fullPrice=100;
+console.log(firstName);
+console.log(age);
+console.log(fullPrice);
+
+{
+     let fullName = "Donald Duck";
+     console.log(fullName);
+}
+
+{    
+     let Age = 26;
+     Age = 98 ;
+     console.log(Age);
+}
+
+{
+     let fullPrice = 200;
+     console.log(fullPrice);
+}
+
+{
+    let character = "Disney"
+    console.log(character);
+}
+
+{
+    let alpha = null;
+    console.log(alpha);
+}
+
+{
+    let zeta = undefined;
+    console.log(zeta);
+}
+
+{
+    let isfollow = false;
+    console.log(isfollow);
+}
+
+{
+    const BETA = 2;
+    console.log(BETA);
+}
+
+{
+    const PI = 3.14;
+    console.log(PI);
+}
+
+    var gamma = "Mickey Mouse"
+    console.log(gamma);
+    
+    var delta = 30;
+    console.log(delta);
+    var delta = 25;
+    var delta = 88;
+    console.log(delta);
+    
+{
+    let $Person_one = "Sylvester";
+    console.log($Person_one);
+}
+
+{
+    let Console = "500" ;
+    console.log(Console) ;
+}
+
+
+
+
+
+
+
+
+
+
