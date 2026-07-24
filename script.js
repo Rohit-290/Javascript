acdc05f0083d3d0a283aa83580cb0967687c8eee
Rohit-290 <rohit290kumar@gmail.com>
2026-07-24 13:33:51 +0530

    Updated om 24th July 2026

--- script.js
+++ script.js
@@ -1,4 +1,53 @@
-//This is a js file
+//This is a js file on operators
 
 /*This is a multi
 line comment*/
+
+let a = 7;
+let b = 5;
+let c = a + b;
+let d = a - b;
+let e = a % b;
+let f = a * b;
+let g = a / b;
+let h = a ** b;
+let z = 2;
+let x = 1;
+
+
+console.log("a+b=" , c);
+console.log("a-b=" , d);
+console.log("a%b=" , e);
+console.log("a*b=" , f);
+console.log("a/b=" , g);
+
+console.log("a**b=" , h);
+console.log("a++=" , a++);
+console.log("b--=" , b--);
+console.log("++a=" , a);
+console.log("--a=" , a);
+
+console.log(a);
+console.log("a+=6" , a=a+6);
+console.log("b-=1" , b=b-1);
+console.log("a*=6" , a=a*6);
+console.log("a%=2" , a=a/2);
+console.log("a**=3" , a=a**3);
+
+console.log("a==x" , a==x);
+console.log("a===z" , a===z);
+console.log("a!=" , a!=z);
+console.log("a!==x" , a!==x);
+console.log("a>z" , a>z);
+console.log("a>=z" , a>=z);
+console.log("a<x" , a<x);
+console.log("a<=x" , a<=x);
+
+
+
+let cond1 = a>b;
+let cond2 = a===b;
+console.log(cond1 && cond2);
+
+
+
