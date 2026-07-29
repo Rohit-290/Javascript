94f486180db26c0958aaae1baf9532915efa859b
Rohit-290 <rohit290kumar@gmail.com>
2026-07-29 11:24:28 +0530

    Updated on 29th July 2026

---
+++ Day 6/practice ques 29-7-26.js
@@ -0,0 +1,45 @@
+let student = {
+           name: "Road Runner",
+           age: 21,
+           cgpa: 7.9
+};
+
+console.log(student["name"]);
+
+
+let name = "     Foghorn Leghorn     ";
+console.log(name.toUpperCase());
+console.log(name.trim());
+
+ 
+let str1 = "Yosemite Sam";
+let str2 = "Wille\nCoyote";
+let str3 = "Pepe\tLe Pew";
+console.log(str1.length);
+console.log(str2.length);
+console.log(str3.length);
+
+console.log(str2);
+console.log(str3);
+
+console.log(str1[5]);
+
+console.log(str1.replace("t","i"));
+console.log(str1.slice(2,6));
+
+
+let obj ={
+
+          color:5,
+          bag:9,
+
+}
+
+let output = `color is ${obj.color} and bag is ${obj.bag}`;
+console.log(output);
+
+let str4 = "porky pig";
+let str5 = "GRANNY";
+console.log(str4+str5);
+console.log(str4.toUpperCase());
+console.log(str5.toLowerCase());
