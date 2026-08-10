460b41268a92c84497c09eb2ef7f960005eb4137
Rohit-290 <rohit290kumar@gmail.com>
2026-08-10 17:13:47 +0530

    Updated on 10th August 2026

--- Day 15/practice ques 10-8-26.js
+++ Day 15/practice ques 10-8-26.js
@@ -1,51 +1,83 @@
-let employee = {
-    taxcal () {
-        console.log("tax rate is 10%");
-            }
-}
+// let employee = {
+//     taxcal () {
+//         console.log("tax rate is 10%");
+//             }
+// }
 
-let AryaStark = {
-    salary : 50000,
-    taxcal () {
-        console.log("tax rate is 20%");
-            }
-}
+// let AryaStark = {
+//     salary : 50000,
+//     taxcal () {
+//         console.log("tax rate is 20%");
+//             }
+// }
 
-AryaStark.__proto__ = employee;
+// AryaStark.__proto__ = employee;
 
+
+// // -----x--------------------------------x----------------------------------x---------------------------------------x--------------------------
+
+
+// class ToyotaCar {
+//     start(){
+//         console.log("start");
+//     }
+//     stop(){
+//         console.log("stop");
+//     }
+//     setBrand(brand){
+//         this.brand = brand;
+//     }
+// }
+
+// let fortuner = new ToyotaCar();
+// fortuner.setBrand = "fortuner";
+
+
+// // -----x--------------------------------x----------------------------------x---------------------------------------x--------------------------
+
+
+// class ToyotaCar {
+//     constructor(brand){
+//         this.brand = brand;
+//     }
+//     start(){
+//         console.log("start");
+//     }
+//     stop(){
+//         console.log("stop");
+//     }
+// }
+
+// let lexus = new ToyotaCar("lexus");
 
 // -----x--------------------------------x----------------------------------x---------------------------------------x--------------------------
 
 
-class ToyotaCar {
-    start(){
-        console.log("start");
-    }
-    stop(){
-        console.log("stop");
-    }
-    setBrand(brand){
-        this.brand = brand;
+class User {
+     constructor(name,email) {
+        this.name = name;
+        this.email = email;
+     }
+     viewData() {
+        console.log("website data");
     }
 }
 
-let fortuner = new ToyotaCar();
-fortuner.setBrand = "fortuner";
+let student = new User("Arya Stark","arya.stark@gmail.com") ;
 
-
+ 
 // -----x--------------------------------x----------------------------------x---------------------------------------x--------------------------
 
 
-class ToyotaCar {
-    constructor(brand){
-        this.brand = brand;
-    }
-    start(){
-        console.log("start");
-    }
-    stop(){
-        console.log("stop");
-    }
+class Admin extends User { 
+      constructor(name,email){
+          super(name,email)
+      } 
+      editData() {
+        super.viewData();
+         console.log("edited data");
+      }
+    
 }
 
-let lexus = new ToyotaCar("lexus");
+let Admin1 = new Admin("admin","admin@123.com");
